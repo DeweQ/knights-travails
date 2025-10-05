615a9257c4fe1348ff3204844c4848e9b3e79944
DeweQ <thedeweq@yandex.ru>
2025-10-05 12:24:08 +0300

    Add main script with showcase of usage

---
+++ main.rb
@@ -0,0 +1,5 @@
+require_relative "lib/knights_travails"
+
+p KnightsTravails.knights_travails([0, 0], [3, 3])
+p KnightsTravails.knights_travails([3, 3], [0, 0])
+p KnightsTravails.knights_travails([0, 0], [7, 7])
